07a5ddc8aa5b465f59840d1cfa7e53ec652a7b71
Juan Manuel Lomas <juanmanuellf@gmail.com>
2016-10-24 12:42:38 +0200

    Estructura base del proyecto

---
+++ app/src/main/java/com/nodry/nodry/Datos/IGasolinerasDAO.java
@@ -0,0 +1,13 @@
+package com.nodry.nodry.Datos;
+
+import java.util.List;
+
+/**
+ * Created by Juan Manuel Lomas on 23/10/2016.
+ */
+
+public interface IGasolinerasDAO {
+
+    List<IGasolinerasDAO> getListGasolineras();
+
+}
---
+++ app/src/main/java/com/nodry/nodry/Negocio/IGestionGasolineras.java
@@ -0,0 +1,14 @@
+package com.nodry.nodry.Negocio;
+
+import com.nodry.nodry.Datos.IGasolinerasDAO;
+
+import java.util.List;
+
+/**
+ * Created by att3mpt on 10/24/16.
+ */
+public interface IGestionGasolineras {
+
+    List<IGasolinerasDAO> getGasolineras();
+
+}
